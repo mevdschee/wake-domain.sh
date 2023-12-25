bec0fb9dd84633971096cf1ed863dc96f5fc10b0
Maurits van der Schee <maurits@vdschee.nl>
2023-12-25 09:44:23 +0100

    Update wake-domain.sh

--- wake-domain.sh
+++ wake-domain.sh
@@ -2,5 +2,6 @@
 tail -F /var/log/auth.log | while read line || { sleep 1 ; continue; }; do
     if [[ $line =~ error:\ connect_to\ ([-a-zA-Z0-9]+)\ port\ 3389 ]]; then
         /usr/bin/virsh start ${BASH_REMATCH[1]}
+        /usr/bin/virsh dompmwakeup ${BASH_REMATCH[1]}
     fi
 done
